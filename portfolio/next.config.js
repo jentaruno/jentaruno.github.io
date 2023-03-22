4019a4478f90ca6996e5668767d187e4bf947abc
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2023-03-21 19:47:47 -0600

    Change base path

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -3,7 +3,7 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
 let assetPrefix = ''
-let basePath = '/portfolio'
+let basePath = ''
 
 if (isGithubActions) {
   // trim off `<owner>/`
