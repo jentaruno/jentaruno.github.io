5158e0d0d479d23e1d685d97c96b8b094e974a8c
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2023-03-21 19:17:50 -0600

    GH Pages config

---
+++ portfolio/next.config.js
@@ -0,0 +1,23 @@
+// next.config.js
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'https://jentaruno.imgix.net/',
+  },
+}
