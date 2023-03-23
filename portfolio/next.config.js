83e57f762da3c87617ad4943942c214791875078
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2023-03-23 17:15:53 -0600

    remove basepath .

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -2,15 +2,15 @@
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = './'
+let assetPrefix = '/'
 let basePath = ''
 
 if (isGithubActions) {
   // trim off `<owner>/`
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-  assetPrefix = `./${repo}/portfolio/`
-  basePath = `./${repo}/portfolio`
+  assetPrefix = `/${repo}/portfolio/`
+  basePath = `/${repo}/portfolio`
 }
 
 module.exports = {
