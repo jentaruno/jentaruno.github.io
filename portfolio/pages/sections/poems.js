60ffbe39634c4ba84fcf4a9250c9a9bdf54772cd
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2023-03-30 23:38:18 -0600

    Book flips

--- portfolio/pages/sections/poems.js
+++ portfolio/pages/sections/poems.js
@@ -5,21 +5,23 @@ export default function Poems({ content }) {
     const maxPage = content.length
     const [currentPage, setCurrentPage] = useState(0)
 
-    function handlePageClick(e, maxPage) {
-        if (e.id < maxPage) {
-            setCurrentPage(currentPage + 1)
-        } else {
-            setCurrentPage(0)
+    function handlePageClick(id, maxPage) {
+        if (id % 2 === 0 && (currentPage + 2) <= maxPage) {
+            setCurrentPage(currentPage + 2)
+            console.log("page" + currentPage)
+        } else if ((currentPage - 2) >= 0) {
+            setCurrentPage(currentPage - 2)
+            console.log("page" + currentPage)
         }
     }
 
     return <>
         <div className={bookStyles.book}>
             <div id="classNames" className={bookStyles.pages}>
-                {content.map(({id, title, desc}) =>
+                {content.map(({id, title, desc}, index) =>
                     (
-                        <div className={`${bookStyles.page} ${currentPage === id ? "flipped" : ""}`} id={id}
-                             onClick={(e) => handlePageClick(e, maxPage)}
+                        <div className={`${bookStyles.page} ${currentPage === index ? "" : `${bookStyles.flipped}`}`} id={index}
+                             onClick={(e) => handlePageClick(index, maxPage)}
                             style={{zIndex: (maxPage - id)}}>
                             {title}
                             <br/>
