1fc4de1590730398621f907b2febaf4a85973f4c
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2023-03-30 23:18:11 -0600

    Try using react hooks to scroll page

--- portfolio/pages/sections/poems.js
+++ portfolio/pages/sections/poems.js
@@ -1,38 +1,26 @@
 import bookStyles from '../../styles/book-scroll.module.css'
-export function processPoems() {
-    const pages = document.getElementsByClassName('page');
-    for (let i = 0; i < pages.length; i++) {
-        const page = pages[i];
-        if (i % 2 === 0) {
-            page.style.zIndex = (pages.length - i);
+import {useState} from "react";
+
+export default function Poems({ content }) {
+    const maxPage = content.length
+    const [currentPage, setCurrentPage] = useState(0)
+
+    function handlePageClick(e, maxPage) {
+        if (e.id < maxPage) {
+            setCurrentPage(currentPage + 1)
+        } else {
+            setCurrentPage(0)
         }
     }
 
-    document.addEventListener('DOMContentLoaded', function () {
-        for (let i = 0; i < pages.length; i++) {
-            //Or var page = pages[i];
-            pages[i].pageNum = i + 1;
-            pages[i].onclick = function () {
-                if (this.pageNum % 2 === 0) {
-                    this.classList.remove('flipped');
-                    this.previousElementSibling.classList.remove('flipped');
-                } else {
-                    this.classList.add('flipped');
-                    this.nextElementSibling.classList.add('flipped');
-                }
-            }
-        }
-    })
-}
-
-export default function Poems({ content }) {
-    console.log(JSON.stringify(content))
     return <>
         <div className={bookStyles.book}>
             <div id="classNames" className={bookStyles.pages}>
-                {content.map(({title, desc}) =>
+                {content.map(({id, title, desc}) =>
                     (
-                        <div className={bookStyles.page}>
+                        <div className={`${bookStyles.page} ${currentPage === id ? "flipped" : ""}`} id={id}
+                             onClick={(e) => handlePageClick(e, maxPage)}
+                            style={{zIndex: (maxPage - id)}}>
                             {title}
                             <br/>
                             {desc}
