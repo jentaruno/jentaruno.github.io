2fc9d11b25a9c1818fc864820139bb896b68ca31
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2023-03-31 10:01:50 -0600

    Styled book font

--- portfolio/pages/sections/poems.js
+++ portfolio/pages/sections/poems.js
@@ -1,12 +1,14 @@
 import bookStyles from '../../styles/book-scroll.module.css'
 import {useState} from "react";
+import utilStyles from "../../styles/utils.module.css";
+import Date from "../../components/date";
 
 export default function Poems({content}) {
     const maxPage = content.length
     const [currentPage, setCurrentPage] = useState(0)
 
-    function handlePageClick(id, maxPage) {
-        if (id % 2 === 0) {
+    function handlePageClick(index) {
+        if (index % 2 === 0) {
             setCurrentPage(currentPage + 2)
         } else {
             setCurrentPage(currentPage - 2)
@@ -30,14 +32,26 @@ export default function Poems({content}) {
     return <>
         <div className={bookStyles.book}>
             <div id="classNames" className={bookStyles.pages}>
-                {content.map(({id, title, desc}, index) =>
+                {content.map(({id, title, date, desc}, index) =>
                     (
                         <div className={`${bookStyles.page} ${setFlippedState(index)}`} id={index}
-                             onClick={(e) => handlePageClick(index, maxPage)}
+                             onClick={() => handlePageClick(index)}
                              style={setZIndex(index, maxPage)}>
-                            {title}
-                            <br/>
-                            {desc}
+                            <div className={bookStyles.pageContent}>
+                                {<div className={utilStyles.listItem} key={id}>
+                                        <div className={utilStyles.listTitle}>
+                                            {title}
+                                        </div>
+                                        <small className={utilStyles.listDate}>
+                                            <Date dateString={date}/>
+                                        </small>
+                                        <br/>
+                                        <small className={utilStyles.listDesc}>
+                                            {desc}
+                                        </small>
+                                    </div>
+                                }
+                            </div>
                         </div>
                     ))}
             </div>
