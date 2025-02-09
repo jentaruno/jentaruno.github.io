c9e9abafdf6df525e5d6d5cfe42e0aef40ec0781
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2025-02-08 20:40:24 -0800

    stronk commands

--- portfolio/static.bat
+++ portfolio/static.bat
@@ -1,6 +1,6 @@
-next build
-next export
+call next build
+call next export
 mkdir .\out\bp-easy-chair
-xcopy ..\..\bp-easy-chair .\out\bp-easy-chair /s/h/y
-touch ./out/.nojekyll
+xcopy ..\bp-easy-chair .\out\bp-easy-chair /s/h/y
+call touch ./out/.nojekyll
 echo 'https://jentaruno.github.io' > ./out/CNAME
