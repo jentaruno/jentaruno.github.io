9bd0c7c2df51aaecd64300f4489587781d5aa69b
jentaruno <jennifertaruno@gmail.com>
2025-12-06 23:56:13 -0800

    scaffolding

--- portfolio/app/drawing/page.tsx
+++ portfolio/app/drawing/page.tsx
@@ -9,6 +9,54 @@ export default function Drawing() {
                     <p>Before college, I taught myself to do lineart and thought that was good enough. When I failed to shade a box in college, I realised I need to practice coloring...</p>
                 </div>
             </div>
+            <div className={'flex flex-col items-center pt-8 pb-24'}>
+                <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                    <div className="grid gap-4">
+                        <div>
+                            <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt=""/>
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt=""/>
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt=""/>
+                        </div>
+                    </div>
+                    <div className="grid gap-4">
+                        <div>
+                            <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt=""/>
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt=""/>
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt=""/>
+                        </div>
+                    </div>
+                    <div className="grid gap-4">
+                        <div>
+                            <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt=""/>
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt=""/>
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt=""/>
+                        </div>
+                    </div>
+                    <div className="grid gap-4">
+                        <div>
+                            <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt=""/>
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt=""/>
+                        </div>
+                        <div>
+                            <img className="h-auto max-w-full rounded-base" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt=""/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
